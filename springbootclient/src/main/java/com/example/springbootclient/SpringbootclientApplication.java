4cce249c563111b46dcfa8bd5f204b3da1846ab9
imran <amran@baseltd.com>
2018-07-10 14:31:12 +0600

    client

---
+++ springbootclient/src/main/java/com/example/springbootclient/SpringbootclientApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootclientApplication.class, args);
+	}
+}
