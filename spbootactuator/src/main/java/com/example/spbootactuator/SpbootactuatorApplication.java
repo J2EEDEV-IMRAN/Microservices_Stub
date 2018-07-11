093475c2c7498eae989041307ad233582340780c
imran <amran@baseltd.com>
2018-07-11 14:49:14 +0600

    actuator

---
+++ spbootactuator/src/main/java/com/example/spbootactuator/SpbootactuatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.spbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootactuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootactuatorApplication.class, args);
+	}
+}
