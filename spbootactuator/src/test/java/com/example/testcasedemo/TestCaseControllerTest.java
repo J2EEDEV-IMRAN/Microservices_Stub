e04a065c67aad7b46d05c181a4ac929afe9da1f4
imran <imran@imran-HP-Pavilion-Laptop-15-cc1xx>
2018-07-13 15:45:57 +0600

    unite testing and integrated testing

--- spbootactuator/src/test/java/com/example/testcasedemo/TestCaseControllerTest.java
+++ spbootactuator/src/test/java/com/example/testcasedemo/TestCaseControllerTest.java
@@ -9,15 +9,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.spbootactuator.SpbootactuatorApplication;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes="TestCaseController.class")
-@AutoConfigureMockMvc
+/*@SpringBootTest(classes=SpbootactuatorApplication.class)
+@AutoConfigureMockMvc*/
+@WebMvcTest
+@ContextConfiguration(classes=SpbootactuatorApplication.class)
 public class TestCaseControllerTest {
 
     @Autowired	
