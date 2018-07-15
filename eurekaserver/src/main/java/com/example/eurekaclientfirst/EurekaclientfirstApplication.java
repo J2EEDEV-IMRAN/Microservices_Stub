c8b4dd87b46c6e2c5c3eb28f7c802de581be5f96
imran <amran@baseltd.com>
2018-07-15 15:36:29 +0600

    eureka server

---
+++ eurekaserver/src/main/java/com/example/eurekaclientfirst/EurekaclientfirstApplication.java
@@ -0,0 +1,14 @@
+package com.example.eurekaclientfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaclientfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaclientfirstApplication.class, args);
+	}
+}
